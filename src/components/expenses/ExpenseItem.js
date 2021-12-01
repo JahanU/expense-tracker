b5bbe05af200b73e119f05fd626cf95ae4c0105d
Jahan <34219176+JahanUoL@users.noreply.github.com>
2021-12-01 22:27:04 +0000

    cleaning up expenseItem

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,23 +1,13 @@
 import ExpenseDate from './ExpenseItemDate';
 import Card from '../ui/Card';
 import './ExpenseItem.css';
-import { useState } from 'react';
 
-function ExpensiveItem(props) {
-
-    const [title, setTitle] = useState(props.expense.title)
-
-    const handleClick = () => {
-        setTitle('New Title!');
-        console.log(title);
-    }
-
+function ExpensiveItem(props) { // stateless/dumb/presentation component 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.expense.date}></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <button onClick={handleClick}> click to update </button>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>£{props.expense.amount}</div>
             </div>
         </Card>
